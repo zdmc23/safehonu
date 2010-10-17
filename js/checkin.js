0183e4f9477c6f4b8fd1cc310b4d2085cecc3ef2
Zachary Mccoy <zdmc23@gmail.com>
2010-10-17 00:50:27 -1000

    link geocode to static map

--- js/checkin.js
+++ js/checkin.js
@@ -8,6 +8,9 @@ $(document).ready(function() {
 			if (debug) alert(position.coords.latitude + ', ' + position.coords.longitude);
 		}); 
 	} 
+	$('a#location').click(function(event) {
+		$('div#map').replaceWith('<iframe src="http://maps.google.com/maps/api/staticmap?markers=size:mid|color:purple|label:!|'+current_position+'&size=500x200&sensor=false"');// width="400" height="300" frameborder="0"></iframe>');
+	});
 	$('input#checkin').click(function(event) {
 		if (debug) alert('current-position: ' + current_position);
 	});
