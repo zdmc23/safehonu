1e81b34973194cbccf8bf15f6722c7ceb077fade
Zachary Mccoy <zdmc23@gmail.com>
2010-10-17 01:29:26 -1000

    at -> @; added some reminders

--- js/checkin.js
+++ js/checkin.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 	var debug = true;
 	var current_position = null;
 	$('div#map').hide();
+	// TODO: hide current position; show spinny wheel
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {  
 			current_position = position.coords.latitude + ',' + position.coords.longitude;
 			$('span#location').replaceWith(current_position);
+			// TODO: hide spinny wheel; show current position
 			if (debug) alert(current_position);
 		}); 
 	} 
@@ -17,6 +19,11 @@ $(document).ready(function() {
 		});
 	});
 	$('input#checkin').click(function(event) {
-		if (debug) alert('current-position: ' + current_position);
+		//if (debug) alert('current-position: ' + current_position);
+		// TODO:
+		// 1. grab username, password, current_position
+		// 2. post to api (alert w/ json)
+		// 3. display notification {success,failure}
+		// 4. disable check-in button
 	});
 });
