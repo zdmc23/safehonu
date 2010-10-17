cf314c711349c6ecad0bdb531e0c2781a0609398
Zachary Mccoy <zdmc23@gmail.com>
2010-10-17 00:38:56 -1000

    rename js files, rename titles, reformat html, begin to incorporate geolocation

--- js/checkin.js
+++ js/checkin.js
@@ -1,31 +1,14 @@
 $(document).ready(function() {
-	$('input#check-in').click(function(event) {
-		alert('check-in!');
-	});
-	/*
-	var current_location = ''; // detect?
+	var debug = true;
+	var current_position = null;
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {  
-			alert(position.coords.latitude + ", " + position.coords.longitude);
+			current_position = position;
+			$('span#location').replaceWith(position.coords.latitude + ', ' + position.coords.longitude);
+			if (debug) alert(position.coords.latitude + ', ' + position.coords.longitude);
 		}); 
 	} 
-	$('input#location').val('1136 Piikoi Place, Honolulu');
-	$('div#meta').hide();
-	$('input#find').click(function(event) {
-		var location = $('input#location').val();
-		if (location != current_location) { 
-			$('div#map').hide('slow', function() {
-				var src = 'http://maps.google.com/maps/api/staticmap?markers=size:mid|color:purple|label:!|'+location+'&size=500x200&sensor=false';
-				$('img#map').attr("src", src).load(function() {
-					current_location = location;
-					$('div#map').show('slow');
-				});
-			});
-		}
+	$('input#checkin').click(function(event) {
+		if (debug) alert('current-position: ' + current_position);
 	});
-	$('a#add-meta').click(function() {
-		$('div#action').hide();
-		$('div#meta').show();
-	});
-	*/
 });
