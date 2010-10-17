dcc6372ab2b14c41c354bc2e79ed9cf0c0e02764
Zachary Mccoy <zdmc23@gmail.com>
2010-10-17 01:02:25 -1000

    fix issue with map by adding an img element that is initially hidden

--- js/checkin.js
+++ js/checkin.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var debug = true;
 	var current_position = null;
+	$('div#map').hide();
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {  
 			current_position = position;
@@ -9,7 +10,11 @@ $(document).ready(function() {
 		}); 
 	} 
 	$('a#location').click(function(event) {
-		$('div#map').replaceWith('<iframe src="http://maps.google.com/maps/api/staticmap?markers=size:mid|color:purple|label:!|'+current_position+'&size=500x200&sensor=false"');// width="400" height="300" frameborder="0"></iframe>');
+		var src = 'http://maps.google.com/maps/api/staticmap?markers=size:mid|color:purple|label:!|'+current_position+'&size=350x300&sensor=false';
+		if (debug) alert('img#map.src: ' + src);
+		$('img#map').attr("src", src).load(function() {
+			$('div#map').show();
+		});
 	});
 	$('input#checkin').click(function(event) {
 		if (debug) alert('current-position: ' + current_position);
