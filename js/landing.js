5818bb5b7e5ffd009e77d7a98d649d7aa87aff77
Zachary Mccoy <zdmc23@gmail.com>
2010-10-17 23:59:54 -1000

    reformat html; embed video at page load; remove unused javascript

--- js/landing.js
+++ js/landing.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-	$('a#video').click(function(event) {
-		$('div#video').replaceWith('<iframe src="http://player.vimeo.com/video/2613544?byline=0&amp;portrait=0&amp;color=c9ffff" width="400" height="300" frameborder="0"></iframe>');
-	});
 	$('input#try').click(function(event) {
 		window.location = 'http://safehonu.com/create';
 	});
