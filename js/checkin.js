7860a77fc0187214cf8faebf1acd69249af9e344
Zachary Mccoy <zdmc23@gmail.com>
2010-10-18 09:58:07 -1000

    forgot the src url

--- js/checkin.js
+++ js/checkin.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 		navigator.geolocation.getCurrentPosition(function(position) {  
 			current_position = position.coords.latitude + ',' + position.coords.longitude;
 			$('img#busy').hide();
+			var src = 'http://maps.google.com/maps/api/staticmap?markers=size:mid|color:purple|label:!|'+current_position+'&size=350x300&sensor=false';
 			$('img#map').attr("src", src).load(function() {
 				$('div#map').show();
 			});
