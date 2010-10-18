fc553f81ea456cf0eecfc1a3fb504d571f9292eb
Zachary Mccoy <zdmc23@gmail.com>
2010-10-17 23:59:01 -1000

    remove debug statement

--- js/checkin.js
+++ js/checkin.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
 	} 
 	$('a#location').click(function(event) {
 		var src = 'http://maps.google.com/maps/api/staticmap?markers=size:mid|color:purple|label:!|'+current_position+'&size=350x300&sensor=false';
-		if (debug) alert('img#map.src: ' + src);
 		$('img#map').attr("src", src).load(function() {
 			$('div#map').show();
 		});
